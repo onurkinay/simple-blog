018d1bf5e5031f4909da9eb056efa43fdba0f733
Onur KINAY <ben@onurkinay.com.tr>
2024-10-12 14:37:28 +0300

    table sb migration

--- database/migrations/2024_10_12_112554_create_categories_table.php
+++ database/migrations/2024_10_12_112554_create_categories_table.php
@@ -3,8 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Category;
-use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->foreignIdFor(Category::class);
-            $table->text("tags")->nullable();
-            $table->foreignIdFor(User::class);
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
--- database/migrations/2024_10_12_113306_create_comments_table.php
+++ database/migrations/2024_10_12_113306_create_comments_table.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Category;
-use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->foreignIdFor(Category::class);
-            $table->text("tags")->nullable();
             $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Post::class);
+            $table->longText('content');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
